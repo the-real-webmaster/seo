4bc5addb04d47d37ff464844482c19d501e14ac9
the-real-webmaster <therealwebmaster.dev@gmail.com>
2026-03-01 13:51:10 +0500

    edit main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const menuBtn = document.querySelector('.header__menu-btn');
 const menuInner = document.querySelector('.header__inner-second');
+const body = document.body;
 
 menuBtn.addEventListener("click", () => {
     menuInner.classList.toggle('active');
+    body.classList.toggle('not-scroll');
 });
